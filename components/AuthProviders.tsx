e6a12425e45c6798afc49e88dcf066f44adeef55
samiksha <samikshawankhade11@gmail.com>
2024-01-25 19:38:10 +0530

    first commit

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+
+  
+
+
+
+const AuthProviders = () => {
+ return(
+    <div>ap</div>
+ )
+}
+
+export default AuthProviders;
